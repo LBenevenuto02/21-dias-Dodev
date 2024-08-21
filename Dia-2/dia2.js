384c4dcb7e6bac8b272449f839b6716a4ba86029
Lucas Benevenuto <benevenuto.lucas02@gmail.com>
2024-08-21 11:13:19 -0300

    criando pasta do dia 2

--- Dia-2/dia2.js
+++ Dia-2/dia2.js
@@ -1,1 +1,26 @@
 console.log('Estou no dia 2, aprendi Git!')
+
+
+let nome1 = ""
+let nome2 = ""
+let idade1 = 0
+let idade2 = 0
+
+nome1 = prompt ("Digite um nome: ")
+nome2 = prompt ("Digite outro nome: ")
+idade1 = prompt ("Digite a idade do primeiro nome: ")
+idade2 = prompt ("Digite a idade do segundo nome: ")
+
+console.log ("O primeiro nome é: " + nome1)
+console.log ("O segundo nome é: " + nome2)
+console.log ("A idade do primeiro nome é: " + idade1)
+console.log ("A idade do segundo nome é: " + idade2)
+console.log ("E a diferença das idades deles é de " + [idade1 - idade2] + " anos")
+
+
+
+
+
+
+
+
